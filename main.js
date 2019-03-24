06b5a873dc317b35f0fbeebf376432310c72a1bb
Samar <samar.sherif@outlook.com>
2019-03-24 23:53:43 +0200

    setting up files

--- main.js
+++ main.js
@@ -4,4 +4,10 @@ $(document).ready(function(){
             $(this).remove();
         });
     });
+
+    $("#add-item").click(function(){
+        var newTodo ="";
+       newTodo =  $("input[type='text']").val();
+       $("ul").append("<li class='list-group-item'>"  + newTodo + " <span><i class='fa fa-trash-alt'></i></span> </li>");
+    });
 });
